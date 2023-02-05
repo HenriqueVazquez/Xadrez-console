b5c0d08d7564d2188f95f315a5f5d2a5b45d9f69
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-02-05 17:15:48 -0300

    controlling the captured pieces

--- Program.cs
+++ Program.cs
@@ -13,10 +13,7 @@ namespace Xadrez_console {
                 while (!partida.Terminada) {
                     try {
                         Console.Clear();
-                        Tela.ImprimirTabuleiro(partida.Tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.Turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+                        Tela.ImprimirPartida(partida);
                         Console.WriteLine();
 
                         Console.Write("Origem: ");
